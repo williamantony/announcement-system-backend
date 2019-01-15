d799259b222f9761b09e8eb8c6f7518fa7abce18
William Antony <william@willsquad.com>
2019-01-14 22:31:13 -0600

    Replaced db.js with knex.js

--- database/db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const db = require('./knexfile.js');
-const knex = require('knex');
-
-const { MODE } = process.env;
-const config = MODE === 'DEV' ? db.development : db.production;
-
-module.exports = knex(config);
