b2aaa6e57f15eeee686c69807fa4e9cdfe89ee62
William Antony <william@willsquad.com>
2019-02-04 22:21:19 -0600

    Updated server.js & routes.js

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,11 @@
 const router = {
+  user: require('./routes/user.route'),
   events: require('./routes/events.route'),
 };
 
 module.exports = server => {
 
+  server.use('/user', router.user);
   server.use('/events', router.events);
 
 };
--- server.js
+++ server.js
@@ -2,20 +2,26 @@ require('dotenv').config();
 
 const {
   PORT,
+  MODE,
 } = process.env;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+
+const routes = require('./api/routes');
 
 const server = express();
 
 server.use(bodyParser.json());
+server.use(cookieParser());
 server.use(cors());
 server.use(morgan('combined'));
 
+routes(server);
 
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${ PORT }`);
+  console.log(`Server running on http://localhost:${ PORT } in ${MODE} mode`);
 });
